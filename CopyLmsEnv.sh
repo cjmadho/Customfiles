2824adb95e0a17d48657f9025a17f1822cbfdb64
risualSupport <30312631+risualSupport@users.noreply.github.com>
2017-09-07 10:03:48 +0100

    Update CopyLmsEnv.sh

--- CopyLmsEnv.sh
+++ CopyLmsEnv.sh
@@ -1,5 +1,3 @@
 mv /edx/app/edxapp/lms.env.json /edx/app/edxapp/lms.env.json.bak
-CD /
-git clone https://github.com/risualSupport/Customfiles.git 
-cp -f /Customfiles/lms.env.json /edx/app/edxapp/lms.env.json 
+cp -f /etc/risualCustom/lms.env.json /edx/app/edxapp/lms.env.json 
 /edx/bin/supervisorctl restart edxapp: 
